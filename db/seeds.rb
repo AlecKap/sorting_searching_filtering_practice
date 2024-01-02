f554600ee797eaf9e8067bf44d8a70f7d0335f95
AlecKap <120225785+AlecKap@users.noreply.github.com>
2024-01-02 13:26:24 -0700

    Add teams and players to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+['Dallas Stars', 'Chicago Blackhawks', 'LA Kings', 'Colorado Avalnache', 'Boston Bruins', 'Toronto Maple Leafs', 'Carolina Hurricanes'].each do |name|
+  Team.create(name: name)
+end
+
+100.times do
+  Player.create(name: Faker::Name.name, team: Team.find(Team.pluck(:id).sample), seasons: rand(25))
+end
